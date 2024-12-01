e7410f56e7e61c04885b61aca1a1e703bc854b35
Perlesvaux <paltryhumanity@gmail.com>
2024-11-30 21:13:19 -0600

    Updates

---
+++ assets/Home-DuKz68Hl.js
@@ -0,0 +1,1 @@
+import{j as e,L as t}from"./index-CTKnFx8C.js";function n({booksAvailable:r,waitScreen:a}){return e.jsx(e.Fragment,{children:e.jsx("div",{className:"book",children:r.map((s,i)=>e.jsxs(t,{to:`/biblio-t-k/${s.url}`,className:"libro",children:[s.title," ",e.jsx("sub",{children:s.author})," ",e.jsx("sub",{children:s.date})]},i))})})}export{n as default};
