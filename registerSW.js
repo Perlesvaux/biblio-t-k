9c5460f8d24a9a97c91ec8471ae1fd85da499a03
Perlesvaux <paltryhumanity@gmail.com>
2025-01-26 07:20:27 -0600

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/bibilioteca-de-don-pelayo/sw.js', { scope: '/bibilioteca-de-don-pelayo/' })})}
