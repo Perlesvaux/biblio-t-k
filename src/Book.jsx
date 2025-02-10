fd58ba0a9f07629f329c764c5983988f4429d79e
Perlesvaux <paltryhumanity@gmail.com>
2025-02-09 23:40:45 -0600

    fix: removed Progress component for UX. Works fine, but it can be distracting.

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,40 +1,23 @@
 import {renderContents, renderFootnotes, renderIndex} from './lib.js' 
 import history from './assets/history.svg'
 import footprints from './assets/footprints.svg'
-import bookmark from './assets/bookmark.svg'
 import LoadingScreen from './LoadingScreen.jsx'
 import AsideMenu from './AsideMenu.jsx'
-import Progress from './Progress.jsx'
 import './Book.css'
-import { useIDB, useYaxis, useProgress } from './lib.js'
-import { useEffect } from 'react'
+import { useIDB, useYaxis } from './lib.js'
 
 export default function Book({ title }) {
 
   const [ book, error, loading ] = useIDB(title, {})
   const ref = useYaxis(title, book)
-  //const [ _y, ref ] = useProgress()
-
-  //useEffect(() => {
-  //  get_y()
-  //
-  //}, [])
-    //<div className={ "bookmark buttonlike" } <img src={bookmark} /> {_y}% </div>
-  
-  
 
   if (error instanceof TypeError) return <div> Oh, you're off the grid. Please go back <strong>On-line</strong> (<i>{error.message}</i>) </div>
   if (error) return <div> (<i>{error.message}</i>) </div>
   if (loading) return <LoadingScreen color="red" taste="dashed" />
   if (!book) return <LoadingScreen color="blue" taste="dashed"> {console.log(book)} </LoadingScreen>
 
-  
-  //isReady()  
-  //loadBookmark()
-
   return ( 
     <main ref={ref}>
-      <Progress book={book}/>
 
       <h1>{book.title}</h1>
 
